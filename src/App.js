1e355d9ff63de6257f318c8c7d62e2713f216319
Tam K.M TRUONG <khacminhtam_truong@protonmail.com>
2020-12-27 23:07:14 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>getting shit done</h1>
+        <p>brrrrrrrrrrr</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
